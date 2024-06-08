c0463f74c8b73a59f60b1d7aeb0d0638b0d39663
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-06-08 15:14:56 +0530

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
